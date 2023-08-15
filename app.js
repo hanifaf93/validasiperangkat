b5ed0e371663a03abead799f875d206897a5bfe5
Ahmad Fauzi Hanif <ahmadfauzihanif@gmail.com>
2023-08-15 19:14:14 +0700

    app.js

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ mysql
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE,
-      port: process.env.PORT,
    })
    .then((connection) => {
       console.log("MySQL Connected");
