9d0c2dd1485b3f4841cb7a10ba662725fc735deb
Ahmad Fauzi Hanif <ahmadfauzihanif@gmail.com>
2023-09-29 06:41:36 +0700

    change mysql Pool Connection

--- app.js
+++ app.js
@@ -21,11 +21,24 @@ dotenv.config({
 (async () => {
   const app = express();
 
-  const db = await mysql.createConnection({
+  // const db = await mysql.createConnection({
+  //   host: process.env.DATABASE_HOST,
+  //   user: process.env.DATABASE_USER,
+  //   password: process.env.DATABASE_PASSWORD,
+  //   database: process.env.DATABASE,
+  // });
+  const db = await mysql.createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
   });
   console.log("MySQL Connected");
 
